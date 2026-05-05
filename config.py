d3437ee43b6f79d369e2a190c20bce5635b8570d
SnehithReddy <nallapuramsnehithreddy@gmail.com>
2026-05-05 12:54:53 +0530

    third commit

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
 import os
 
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "dev-secret-key"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+SQLALCHEMY_TRACK_MODIFICATIONS = False
