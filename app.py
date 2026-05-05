49976117401a315777efcbb745604a8741b6bb8b
SnehithReddy <nallapuramsnehithreddy@gmail.com>
2026-05-05 12:21:34 +0530

    changes in backend

--- app.py
+++ app.py
@@ -37,5 +37,5 @@ def static_files(path):
 
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all()   # creates tables in MySQL
-    app.run(debug=True)
+        db.create_all()   
+    app.run()
