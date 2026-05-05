df0a069cfd4b617677a678450cfb11fd9ae32f51
SnehithReddy <nallapuramsnehithreddy@gmail.com>
2026-05-05 13:14:54 +0530

    changes

--- config.py
+++ config.py
@@ -1,9 +1,4 @@
-import os
-
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "dev-secret-key"
-
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
-        "mysql://root:mkFbJUcAPgXXBvokNPiOCbUGzWpgjEvw@trolley.proxy.rlwy.net:12876/railway"
-
+    SECRET_KEY = "dev-secret-key"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
