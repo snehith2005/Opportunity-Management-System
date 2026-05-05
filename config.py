cb88bd86a1ec95865b906a634ce48a8a28952326
SnehithReddy <nallapuramsnehithreddy@gmail.com>
2026-05-05 13:07:49 +0530

    config changes

--- config.py
+++ config.py
@@ -2,5 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "dev-secret-key"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
+        "mysql://root:mkFbJUcAPgXXBvokNPiOCbUGzWpgjEvw@trolley.proxy.rlwy.net:12876/railway"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
