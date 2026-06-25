4192d4364cf9b147322898871d515aea27b620bf
SnehithReddy <nallapuramsnehithreddy@gmail.com>
2026-06-25 22:34:19 +0530

    database url change

--- config.py
+++ config.py
@@ -1,11 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
-
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL",
-        "mysql+pymysql://root:root@localhost:3306/admin_portal"
-    )
-
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
